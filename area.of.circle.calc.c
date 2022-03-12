aa6f1c3a5026d83867aab780f4ed6a2fddfbb8bb
sidd9308 <87959922+sidd9308@users.noreply.github.com>
2022-03-12 09:19:32 +0530

    Add files via upload

---
+++ area.of.circle.calc.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+ 
+int main () {
+  int radius;
+
+  printf("Enter Radius");
+  scanf("%d", &radius);
+  printf("The Area Is %d", radius * 2);
+  return 0;
+}
---
+++ area.of.square.calc.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+   int main () {
+     int side;
+
+     printf("Enter Side");
+     scanf("%d", &side);
+     printf(" The Side is %d", side * side);
+     return 0;
+   }
