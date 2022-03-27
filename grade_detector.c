683f313fc600194f722947d67cba1cd0abaa5ed8
sidd9308 <87959922+sidd9308@users.noreply.github.com>
2022-03-27 09:25:11 +0530

    Update grade_detector.c

--- grade_detector.c
+++ grade_detector.c
@@ -7,15 +7,15 @@ int main() {
     scanf("%d", &marks);
 
     if(marks<50){
-        printf("Fail, Pitoge bahut apni mummy se \n");
+        printf("Fail \n");
     }else if(marks>50 && marks<60){
-        printf("D Grade, Tum Bhi Pitoge \n");
+        printf("D Grade \n");
     }else if(marks>60 && marks<75){
-        printf("C Grade, 50-50 Chance hai pitne ke \n");
+        printf("C Grade \n");
     }else if(marks>75 && marks<90){
-        printf("B Grade, Kam se kam maar nahi khaaoge mummy se \n");
+        printf("B Grade \n");
     }else if(marks>90 && marks<100){
-        printf("A Grade, Tumhari Mummy tumhe appreciate karengi \n");
+        printf("A Grade \n");
     }else if(marks>100){
         printf("Invalid \n");
     }
